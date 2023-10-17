0d5717dcf77b2ebc505bc4ef620fd6cf2acf66f3
TravisM74 <117643403+TravisM74@users.noreply.github.com>
2023-10-17 17:05:59 +0300

    player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float horizontal = 0;
+    private float vertical = 0;
+    private float speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,12 +16,16 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // Read input from player
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+    void Update() {
+        ReadInput();
+        Vector3 movement = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
+        transform.position += movement;
+       
+    }
 
-        Debug.Log($"horizontal {horizontal}  vertical{vertical}");
+    private void ReadInput() {
+        // Read input from player
+        horizontal += Input.GetAxis("Horizontal");
+        vertical += Input.GetAxis("Vertical");
     }
 }
